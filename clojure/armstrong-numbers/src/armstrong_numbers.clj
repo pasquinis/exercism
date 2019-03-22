ede4e26dd1246407614b69e6af39b28cb7ab7dca
Simone Pasquini <simone.pasquini@gmail.com>
2019-03-22 15:52:17 +0100

    Armstrong Numbers remove temporary variables

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,11 +1,9 @@
 (ns armstrong-numbers)
 
 (defn armstrong? [number]
-  (let [string-number (str number)
-        string-number-length (count string-number)]
-    (= (double number)
-       (->> string-number
-            (map str)
-            (map read-string)
-            (map (fn [a] (Math/pow a string-number-length)))
-            (reduce + 0)))))
+  (= (double number)
+     (->> (str number)
+          (map str)
+          (map read-string)
+          (map (fn [a] (Math/pow a (count (str number)))))
+          (reduce + 0))))
